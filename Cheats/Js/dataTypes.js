29ea466529d46625ce2487dc602633ecdef70920
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-02 10:42:16 +0100

    first commit

---
+++ Cheats/Js/dataTypes.js
@@ -0,0 +1,21 @@
+// Data types in JavaScript
+
+// STRINGS
+
+const myString = 'hello world';
+
+// NUMBERS
+
+const myNumber = 7
+
+// BOOLEAN 
+
+const myBoolean = true
+
+// NULL
+
+const myNull = null
+
+// UNDEFINED
+
+const empty = undefined
