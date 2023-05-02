2bb652fa430514537d596840cc8cb5edfa59fb98
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-02 10:55:41 +0100

    primitive datatypes

--- Cheats/Js/dataTypes.js
+++ Cheats/Js/dataTypes.js
@@ -1,21 +1,39 @@
 // Data types in JavaScript
 
-// STRINGS
+// STRINGS is used to store text
 
 const myString = 'hello world';
 
-// NUMBERS
+// NUMBERS represents integer and floating numbers (decimals and exponentials).
 
-const myNumber = 7
+const numb1 = 7
+const numb2 = 10
+console.log(numb1) // 7
 
-// BOOLEAN 
+// BOOLEAN represents one of two values: true or false
 
-const myBoolean = true
+const isTrue = true
+const isFalse = false
+console.log(isFalse) //false
 
-// NULL
+// NULL is a special value that represents empty or unknown value
 
-const myNull = null
+const unknown = null
+console.log (unknown) //null
 
-// UNDEFINED
+// UNDEFINED If a variable is declared but the value is not assigned, then the value of that variable will be undefined
 
-const empty = undefined
+let student;
+const student2 = undefined
+console.log(student) //undefined
+
+
+// OBJECTS An object is a complex data type that allows us to store collections of data
+
+const user = {
+    firstName: 'john',
+    lastName: null,
+    age: 10
+};
+console.log(user)// john , null , 10
+
