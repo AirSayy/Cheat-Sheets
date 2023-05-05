0c6710bebec63e2e9f359b9a0af9c3a37fff3485
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-05 12:14:42 +0100

    includes string method

--- Cheats/Js/stringMethods.js
+++ Cheats/Js/stringMethods.js
@@ -13,24 +13,29 @@ console.log(thisString.charCodeAt(0))  //73
 console.log(thisString.charCodeAt(3))  //109
 
 
-// concat : Joins two or more strings and returns a new string
+// concat() : Joins two or more strings and returns a new string
 const thisString = 'I am a software developer.'
 const secondString = 'Hire me'
 
 console.log(thisString.concat(secondString))  // I am a software developer.Hire me
 
 
-// endsWith : Checks if a string ends with a specified string or character
+// endsWith() : Checks if a string ends with a specified string or character
 const thisString = 'I am a software developer.'
 
 console.log(thisString.endsWith('software'))   //false
 console.log(thisString.endsWith('developer.'))   //true
 
 
-// fromCharCode : This converts unicode values to characters
+// fromCharCode() : This converts unicode values to characters
 const thisString = 'I am a software developer.'
 
 console.log(String.fromCharCode(73))   //"I"
 console.log(String.fromCharCode(109))  //"m"
 
 
+// includes() : This checks if the string contains the specified string or character.
+const thisString = 'I am a software developer.'
+
+console.log(thisString.includes('y'))  //false
+console.log(thisString.includes('t'))  //true
