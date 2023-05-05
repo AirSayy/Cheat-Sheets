7b531ae858279d7f748d1621146d5940bce74d15
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-05 11:23:51 +0100

    string methods

--- Cheats/Js/stringMethods.js
+++ Cheats/Js/stringMethods.js
@@ -1,1 +1,17 @@
-const stringOne = ""
+// String Methods in JavaScript
+
+
+// charAt() : Returns a character at a specified index/position.
+const thisString = 'I am a software dveloper'
+console.log(thisString.charAt(0))  //"I"
+console.log(thisString.charAt(3))  //"m"
+
+
+// charCodeAt() : Returns a unicode of the character at a specific index
+const thisString = 'I am a software dveloper'
+console.log(thisString.charCodeAt(0))  //73
+console.log(thisString.charCodeAt(3))  //109
+
+
+
+
